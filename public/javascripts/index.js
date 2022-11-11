6f39de7e1f3fb06d2c8ffaa09cb286b774568bfb
LakhderAmine99 <amine.lakhder@usmba.ac.ma>
2022-11-11 02:46:29 +0100

    update ui files

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,23 +2,48 @@ import Request from './modules/request/request.js';
 
 function ArabicAlphabetsSignLanguage(){
 
-    const uploadArea = document.getElementById('upload-area')
+    const uploadArea = document.getElementById('uploadBtn');
+
+    const imageSurface = document.querySelector('.image');
+
+    // var video = document.querySelector("#videoElement");
+
 
     this.start = function(){
 
         console.log("ArabicAlphabetsSignLanguage Application is up and running...");
 
         initEventListeners();
+
+        // if (navigator.mediaDevices.getUserMedia) {
+        //     navigator.mediaDevices.getUserMedia({ video: true })
+        //       .then(function (stream) {
+        //         video.srcObject = stream;
+        //       })
+        //       .catch(function (err0r) {
+        //         console.log("Something went wrong!");
+        //       });
+        //   }
     }
 
     function initEventListeners(){
 
-        uploadArea.addEventListener('click',uploadHandler);
+        uploadArea.addEventListener('change',uploadHandler);
     }
 
-    async function uploadHandler(){
+    async function uploadHandler(e){
 
-        alert('Upload me')
+        imageSurface.innerHTML = "";
+
+        let src = URL.createObjectURL(e.target.files[0]);
+
+        let img = new Image();
+
+        img.src = src;
+        img.width = 300;
+        img.height = 300;
+
+        imageSurface.append(img);
     }
 }
 
